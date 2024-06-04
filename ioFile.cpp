78f2c1011d510105a78e19716455c4b759245b3f
ahmadfauzanisnata <fauzanisnata56@gmail.com>
2024-06-04 14:35:00 +0700

    membuat fungsi unlimited loop untuk menulis

--- ioFile.cpp
+++ ioFile.cpp
@@ -10,4 +10,10 @@ int main() {
     // menunjuk ke sebuah nama file
     outfile.open("contohfile.txt");
     cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+    //unlimited loop untuk menulis
+    while (true) {
+        cout << "- ";
+        
+    }
 }
